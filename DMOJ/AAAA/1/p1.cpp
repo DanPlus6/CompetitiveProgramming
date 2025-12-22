90848afa2447e667d282b67afac40fba2b48cbf8
dave <d4nplus6@gmail.com>
2025-12-22 14:11:18 -0500

    finished AAAA1 :|

--- DMOJ/AAAA/1/p1.cpp
+++ DMOJ/AAAA/1/p1.cpp
@@ -6,17 +6,17 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int N; cin >> N;
-    vector<int> A(N);
     int even = 0;
     int odd = 0;
     for (int i = 0; i < N; i++) {
         int tmp; cin >> tmp;
-        A[i] = tmp;
         if (!(tmp & 1)) {even++;}
         else {odd++;}
     }
 
-    
+    if (N & 1) {cout << "Steven" << endl; return 0;}
+    if (!odd) {cout << "Steven" << endl; return 0;}
+    if (even >= N/2) {cout << "Steven" << endl; return 0;}
 
     cout << "Todd" << endl;
     return 0;
