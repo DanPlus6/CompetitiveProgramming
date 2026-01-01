16ea12ec57a6d67e52d8c4001b772dd057b362d6
dave <d4nplus6@gmail.com>
2026-01-01 13:07:45 -0500

    happy new year!

---
+++ DMOJ/Bulgarian OI/2009/p5/build.py
@@ -0,0 +1,1 @@
+with open("/home/$USER/Work/CS/Personal/CompetitiveProgramming/DMOJ/Bulgarian OI/2009/p5/troll.txt","r") as f: print(f.readlines())
--- DMOJ/Bulgarian OI/2009/p5/troll.py
+++
@@ -1,7 +0,0 @@
-n = 0
-i = 1
-while n <= 250:
-    if set(str(i**2)) == {'0','4','9'}:
-        n+=1
-        with open('Bulgarian OI/2009/p5/troll.txt','a') as f: f.write(str(i)+'\n')
-    i += 1
