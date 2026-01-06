c558f4b80da075f6a908f1a6c4f23c8810693c9c
dave <d4nplus6@gmail.com>
2026-01-05 20:22:46 -0500

    added CCC '10 J1 sol

---
+++ DMOJ/CCC/2010/ccc10j1.cpp
@@ -0,0 +1,23 @@
+// CCC '10 J1 - What is n, Daddy?
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N; cin >> N;
+    switch (N) {
+        case 1: cout << "1\n"; break;
+        case 2: cout << "2\n"; break;
+        case 3: cout << "2\n"; break;
+        case 4: cout << "3\n"; break;
+        case 5: cout << "3\n"; break;
+        case 6: cout << "3\n"; break;
+        case 7: cout << "2\n"; break;
+        case 8: cout << "2\n"; break;
+        case 9: cout << "1\n"; break;
+        case 10: cout << "1\n"; break;
+    }
+    
+    return 0;
+}
