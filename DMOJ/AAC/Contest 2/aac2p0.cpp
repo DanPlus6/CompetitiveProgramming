03e8894178c58b59195f44011695ba1e9794fda0
dave <d4nplus6@gmail.com>
2026-01-12 21:21:16 -0500

    added c++ sol for An Animal Contest 2 Problem 0

---
+++ DMOJ/AAC/Contest 2/aac2p0.cpp
@@ -0,0 +1,12 @@
+// An Animal Contest 2 P0 - Koala Matchmaking
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N; cin >> N;
+    cout << N-1 << "\n";
+    
+    return 0;
+}
