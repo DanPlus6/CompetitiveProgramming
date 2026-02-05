70fa890ba5d820de5bb685cdceb97cf647eb58a8
david <d4nplus6@gmail.com>
2026-02-05 09:27:00 -0500

    added c++ solution template

---
+++ DMOJ/CCC/template.cpp
@@ -0,0 +1,42 @@
+// __problem_name__
+#include <bits/stdc++.h>
+using namespace std;
+char alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+typedef long long ll;
+typedef pair <int, int> pi;
+typedef pair<pi, int> pii;
+typedef pair <ll, int> pli;
+typedef pair<ll, ll> pl;
+typedef pair <ll,pl> pll;
+typedef pair <string, int> psi;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vii;
+typedef vector<pl> vll;
+typedef vector<pii> viii;
+#define endl '\n'
+#define FILL(a, b) memset(a, b, sizeof(a))
+#define ALL(x) x.begin(), x.end()
+#define ILOOP(n) for (int i = 0; i < n; i++)
+#define CLOOP(s) for (char c: s)
+#define pb push_back
+#define FOUNDANS(res) {cout << res << endl; return 0;}
+
+const int MM = 1e5+5;
+const int INF = 0x3F3F3F3F;
+const ll INFL = 0x3F3F3F3F3F3F3F3FLL;
+const int MOD = 1e9 + 7;
+
+
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    // code here
+
+    return 0;
+}
