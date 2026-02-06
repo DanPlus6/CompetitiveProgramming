0db9d717b906f533974e63956cbee53f3fa2379b
david <d4nplus6@gmail.com>
2026-02-05 21:45:51 -0500

    added primary, secondary, and reverse integer loops to c++ template

--- DMOJ/CCC/template.cpp
+++ DMOJ/CCC/template.cpp
@@ -22,6 +22,9 @@ typedef vector<pii> viii;
 #define FILL(a, b) memset(a, b, sizeof(a))
 #define ALL(x) x.begin(), x.end()
 #define ILOOP(n) for (int i = 0; i < n; i++)
+#define PRIMLOOP(N) for (int n = 0; n < N; n++)
+#define SECLOOP(M) for (int m = 0; m < M; m++)
+#define RLOOP(n) for (int i = n; i >= 0; i--)
 #define CLOOP(s) for (char c: s)
 #define pb push_back
 #define FOUNDANS(res) {cout << res << endl; return 0;}
