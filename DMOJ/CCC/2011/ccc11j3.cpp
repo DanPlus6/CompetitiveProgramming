a3fe9c7439ea9ba7d10571bf1b4d4b83eb612591
david <d4nplus6@gmail.com>
2026-02-13 20:53:10 -0500

    added C++ sol for CCC '11 J3

---
+++ DMOJ/CCC/2011/ccc11j3.cpp
@@ -0,0 +1,54 @@
+// CCC '11 J3 - Sumac Sequences
+#include <bits/stdc++.h>
+using namespace std;
+char alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<pi, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pl;
+typedef pair<ll,pl> pll;
+typedef pair<string, int> psi;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vii;
+typedef vector<pl> vll;
+typedef vector<pii> viii;
+typedef vector<bool> vb;
+typedef priority_queue<int, vi> ipq;
+typedef priority_queue<int, vi, greater<int>> ripq;
+#define endl '\n'
+#define FILL(a, b) memset(a, b, sizeof(a))
+#define ALL(x) x.begin(), x.end()
+#define ILOOP(n) for (int i = 0; i < n; i++)
+#define PRIMLOOP(N) for (int n = 0; n < N; n++)
+#define SECLOOP(M) for (int m = 0; m < M; m++)
+#define RLOOP(n) for (int i = n; i >= 0; i--)
+#define CLOOP(s) for (char c: s)
+#define pb push_back
+#define FOUNDANS(res) {cout << res << endl; return 0;}
+
+constexpr int MM = 1e5+5;
+constexpr int INF = 0x3F3F3F3F;
+constexpr ll INFL = 0x3F3F3F3F3F3F3F3FLL;
+constexpr int MOD = 1e9 + 7;
+
+
+int sumacseq(int T1, int T2, int terms) {
+    if (T1 < T2) return terms+1;
+    return sumacseq(T2, (T1-T2), (terms+1));
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int T1,T2; cin >> T1 >> T2;
+    if (T1 < T2) FOUNDANS(1)
+    cout << sumacseq(T2,T1-T2,2) << endl;
+
+    return 0;
+}
