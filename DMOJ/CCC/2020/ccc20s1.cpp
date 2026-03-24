2f82ce236d22f2c7dd045a93377a202bf0505a4f
david <d4nplus6@gmail.com>
2026-03-23 21:00:28 -0400

    optimized C++ sol for CCC '20 S1

--- DMOJ/CCC/2020/ccc20s1.cpp
+++ DMOJ/CCC/2020/ccc20s1.cpp
@@ -1,38 +1,25 @@
 // CCC '20 S1 - Surmising a Sprinter's Speed
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
+
+#define ALL(x) x.begin(), x.end()
+#define endl '\n'
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    long long N;
-    cin >> N;
+    int N; cin >> N;
 
-    // long long t,d;
-    // cin >> t >> d;
-    
-    // double res = 0.0;
-    // for (long long i = 1; i < N; i++) {
-    //     long long ct, cd;
-    //     cin >> ct >> cd;
-
-    //     if (ct != t) { res = max(abs((double)cd-d)/abs((double)ct-t),res); };
-    //     t = ct; d = cd;
-    // }
-
-    vector<vector<long long>> arr(N, vector<long long>(2));
-    for (long long i = 0; i < N; i++) {
-        long long t,d;
-        cin >> t >> d;
-        arr[i] = {t, d};
+    vector<pair<int,int>> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i].first >> arr[i].second;
     }
 
-    sort(arr.begin(), arr.end());
+    sort(ALL(arr));
 
     double res = 0.0;
-    for (long long i = 0; i < N-1; i++) {
-        res = max(abs((double)arr[i][1]-arr[i+1][1])/abs((double)arr[i][0]-arr[i+1][0]),res);
+    for (int i = 0; i < N-1; i++) {
+        res = max(abs((double)arr[i].second-arr[i+1].second)/abs((double)arr[i].first-arr[i+1].first), res);
     }
 
     cout << res << endl;
