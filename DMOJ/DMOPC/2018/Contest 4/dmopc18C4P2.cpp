7f9cea072460e032459833de57cca302fa431dcd
dave <d4nplus6@gmail.com>
2026-04-05 13:22:07 -0400

    cleaned up old code and comments in DMOPC '18 Contest 4 P2

--- DMOJ/DMOPC/2018/Contest 4/dmopc18C4P2.cpp
+++ DMOJ/DMOPC/2018/Contest 4/dmopc18C4P2.cpp
@@ -5,19 +5,10 @@ using namespace std;
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
 
-// typedef vector<int> vi;
-// typedef vector<bool> vb;
-// #define endl '\n'
-// #define ALL(x) x.begin(), x.end()
 #define PRIMLOOP(N) for (int i = 0; i < N; i++)
-
-// constexpr int INF = 0x3F3F3F3F;
-
 
 
 static bitset<1000005> dp;
-
-/* orz relyks */
 int main() {
     int N; scanf("%d", &N);
 
@@ -44,32 +35,3 @@ int main() {
 
     return 0;
 }
-
-
-// /* Orz bruce */
-// int main() {
-//     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-
-//     int N; cin >> N;
-//     vi A(N); int total = 0;
-//     PRIMLOOP(N) {cin >> A[i]; total += A[i];}
-    
-//     vb dp(total+1,false);
-    
-//     dp[0] = true;
-//     PRIMLOOP(N) {
-//         for (int s = total; s >= A[i]; s--) {
-//             int peek = s - A[i];
-//             if (dp[peek]) dp[s] = true;
-//         }
-//     }
-
-//     int res = INF;
-//     for (int s = 0; s <= total; s++) {
-//         if (dp[s]) res = min(res, abs(total - 2*s));
-//     }
-
-//     cout << res << endl;
-
-//     return 0;
-// }
