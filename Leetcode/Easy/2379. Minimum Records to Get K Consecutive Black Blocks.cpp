7efff9c3d0103fe48028dc18efa5ee63c9896ff6
dave <d4nplus6@gmail.com>
2026-04-16 18:26:04 -0400

    added C++ sol for Leetcode 2379

---
+++ Leetcode/Easy/2379. Minimum Records to Get K Consecutive Black Blocks.cpp
@@ -0,0 +1,30 @@
+// 2379. Minimum Records to Get K Consecutive Black Blocks.cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int INF = 0x3F3F3F3F;
+
+
+class Solution {
+public:
+    static int minimumRecolors(string blocks, int k) {
+        blocks += ' ';
+
+        int black = 0, white = 0;
+        int res = INF;
+        for (int i = 0; i < blocks.size(); i++) {            
+            if (i >= k) {
+                if (!white) return 0;
+                res = min(res, white);
+
+                if (blocks[i-k] == 'B') black--;
+                else white--;
+            }
+
+            if (blocks[i] == 'B') black++;
+            else white++;
+        }
+
+        return res;
+    }
+};
