b84f7986f27f3d6511b431f43f3353f2059660db
dave <d4nplus6@gmail.com>
2026-04-18 22:36:49 -0400

    fixed leetcode 14 C++ sol

--- Leetcode/Easy/14. Longest Common Prefix.cpp
+++ Leetcode/Easy/14. Longest Common Prefix.cpp
@@ -7,9 +7,13 @@ class Solution {
 public:
     static string longestCommonPrefix(const vector<string>& strs) {
         string res = "";
+
+        if (strs.size() == 1) return strs[0];
+
         for (int it = 0;;it++) {
             char c = '\0';
             for (const string &s: strs) {
+                if (!s[it]) return res;
                 if (!c) c = s[it];
                 else if (s[it] != c) return res;
             }
