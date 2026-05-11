aa884ced5f59e1e792ac58e337d9de23f40843a9
dave <d4nplus6@gmail.com>
2026-05-11 13:24:02 -0400

    fixed and optimized C++ sol for Leetcode 27

--- Leetcode/Easy/27.RemoveElement.cpp
+++ Leetcode/Easy/27.RemoveElement.cpp
@@ -4,18 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int res = 0;
-        int left = 0, right = nums.size()-1;
-        for (int left = 0, right = nums.size()-1; left < right && left < nums.size(); left++) {
-            if (nums[left] == val) {
-                swap(nums[left], nums[right]);
-                --right;
-                ++res;
+    static int removeElement(vector<int>& nums, int val) {
+        int left = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[left] = nums[i];
+                ++left;
             }
         }
-
-        for (int i = 0; i < nums.size()-1-right; i++) nums.pop_back();
-        return res;
+        
+        return left;
     }
 };
