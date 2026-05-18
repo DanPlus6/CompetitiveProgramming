3aeea428ca173f97eb6be8cc519a3f2c68a82422
dave <d4nplus6@gmail.com>
2026-05-17 23:22:55 -0400

    added C++ boilerplate for technical online judges

---
+++ z - boilerplates/C++/tcoj_boilerplate.cpp
@@ -0,0 +1,30 @@
+// __problem_name__
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef tuple<int ,int, int> ti;
+typedef pair<pi, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pl;
+typedef tuple<ll,ll,ll> tl;
+typedef pair<ll,pl> pll;
+typedef vector<int> vi;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef queue<int> qi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef vector<pi> vii;
+typedef vector<ti> vti;
+typedef vector<pl> vll;
+typedef vector<tl> vtl;
+typedef vector<bool> vb;
+#define FILL(a, b) memset(a, b, sizeof(a))
+#define ALL(x) x.begin(), x.end()
+#define LOOP(n) for (int _ = 0; _ < n; _++)
+#define pb push_back
+
+// solution here
