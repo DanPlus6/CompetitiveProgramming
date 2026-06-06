19382b86f96b8425111dd7e73b0292cb5ce8d80e
dave <d4nplus6@gmail.com>
2026-06-05 22:48:11 -0400

    Added C++ sol for Leetcode 3635

---
+++ Leetcode/Medium/3635. Earliest Finish Time for Land and Water Rides II.cpp
@@ -0,0 +1,26 @@
+// 3635. Earliest Finish Time for Land and Water Rides II
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int land = INT_MAX, water = land, res = land;
+
+        for (int i = 0; i < landDuration.size(); i++) {
+            land = min(land, landStartTime[i] + landDuration[i]);
+        }
+
+        for (int i = 0; i < waterDuration.size(); i++) {
+            water = min(water, waterStartTime[i] + waterDuration[i]);
+            res = min(res, max(land, waterStartTime[i]) + waterDuration[i]);
+        }
+
+        for (int i = 0; i < landDuration.size(); i++) {
+            res = min(res, max(water, landStartTime[i]) + landDuration[i]);
+        }
+
+        return res;
+    }
+};
+
