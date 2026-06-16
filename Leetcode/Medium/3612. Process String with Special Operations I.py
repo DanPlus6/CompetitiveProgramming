b69c8fed6d9eae64545046e25b3a3b147770c7da
dave <d4nplus6@gmail.com>
2026-06-16 15:41:11 -0400

    Optimized PY3 sol for Leetcode 3612

--- Leetcode/Medium/3612. Process String with Special Operations I.py
+++ Leetcode/Medium/3612. Process String with Special Operations I.py
@@ -2,15 +2,11 @@
 
 class Solution:
     def processStr(self, s: str) -> str:
-        res = []
+        res = ""
         for c in s:
-            if c == '*': 
-                try: res.pop()
-                except IndexError: continue
-            elif c == '#': 
-                for i in range(len(res)):
-                    res.append(res[i])
+            if c == '*' and len(res) >= 1: res = res[:-1]
+            elif c == '#': res += res
             elif c == '%': res = res[::-1]
-            else: res.append(c)
+            elif c.isalpha(): res += c
         
-        return ''.join(res)
+        return res
