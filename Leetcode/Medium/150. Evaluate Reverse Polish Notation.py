f02893d40899e00076317671c69242b97f889ace
dave <d4nplus6@gmail.com>
2026-06-21 13:05:18 -0400

    Optimized PY3 sol for Leetcode 150

--- Leetcode/Medium/150. Evaluate Reverse Polish Notation.py
+++ Leetcode/Medium/150. Evaluate Reverse Polish Notation.py
@@ -1,28 +1,17 @@
 # 150. Evaluate Reverse Polish Notation
 
 class Solution:        
-    def evalRPN(self, tokens: list[str]) -> int:
-        def isInt(x):
-            try:
-                int(x)
-                return True
-            except ValueError:
-                try:
-                    float(x)
-                    return True
-                except ValueError:
-                    return False
-                
+    def evalRPN(self, tokens: list[str]) -> int:           
         stk = []
         for token in tokens:
-            if isInt(token): stk.append(token)
+            if token not in {'+','-','*','/'}: stk.append(int(token))
             else:
-                second = int(stk.pop())
-                first = int(stk.pop())
+                second = stk.pop()
+                first = stk.pop()
 
                 if token == '+': stk.append(first + second)
                 if token == '-': stk.append(first - second)
                 if token == '*': stk.append(first * second)
-                if token == '/': stk.append(first / second)
+                if token == '/': stk.append(int(first / second))
 
-        return int(stk.pop())
+        return stk[0]
