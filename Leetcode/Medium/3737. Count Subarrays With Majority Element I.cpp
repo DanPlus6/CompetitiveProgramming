d37ebdfaec7398294214326e412fa93fae01564b
dave <d4nplus6@gmail.com>
2026-06-25 18:21:26 -0400

    Optimized Leetcode 3737 C++ sol with PSA

--- Leetcode/Medium/3737. Count Subarrays With Majority Element I.cpp
+++ Leetcode/Medium/3737. Count Subarrays With Majority Element I.cpp
@@ -8,13 +8,17 @@ typedef vector<int> vi;
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
+        vi psa(nums.size(), 0); {
+            psa[0] = (nums[0]==target)?1:0;
+            for (int i = 1; i < nums.size(); ++i)
+                psa[i] = psa[i-1] + ((nums[i]==target) ? 1 : 0);
+        }
+
         ll res = 0;
-        vi subarr;
         for (int ct = 0, i = 0; i < nums.size(); ++i) {
             ct = 0;
             for (int len, j = i; j < nums.size(); ++j) {
-                if (nums[j] == target) ++ct;
-
+                ct = psa[j] - ((i>0) ? psa[i-1] : 0);
                 if ((ct*2) > (j-i+1)) ++res;
             }
         }
