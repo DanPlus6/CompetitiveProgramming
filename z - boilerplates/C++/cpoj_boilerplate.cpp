67655718dd98c558cd3eddf676b1ff2c20cca9c7
dave <d4nplus6@gmail.com>
2026-07-06 22:20:47 -0400

    Added back alphabet and pragma optimize macros to C++ boilerplate

--- z - boilerplates/C++/cpoj_boilerplate.cpp
+++ z - boilerplates/C++/cpoj_boilerplate.cpp
@@ -1,6 +1,10 @@
 // __problem_name__
 #include <bits/stdc++.h>
 using namespace std;
+char alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
 
 typedef long long ll;
 typedef pair<int, int> pi;
