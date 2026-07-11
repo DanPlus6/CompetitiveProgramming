1f53a16cde09d47b6915b3ca2989cd23235d4946
dave <d4nplus6@gmail.com>
2026-07-11 13:10:26 -0400

    Redid C++ sol for DMOPC '16 C4 P2

--- DMOJ/DMOPC/2016/Contest 4/DMOPC '16 Contest 4 P2 - Systests.cpp
+++ DMOJ/DMOPC/2016/Contest 4/DMOPC '16 Contest 4 P2 - Systests.cpp
@@ -2,66 +2,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair <int, int> pi;
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+typedef long long ll;
 typedef vector<int> vi;
-#define endl '\n'
 #define ALL(x) x.begin(), x.end()
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-    int B; cin >> B;
+    int N; scanf("%d",&N);
+    vi Fi(N), Ei(N), Pi(N);
     int res = 0;
-    vi fv(B), ev(B), pv(B);
-    for (int b = 0; b < B; b++) {
-        cin >> fv[b] >> ev[b] >> pv[b];
-        res += pv[b];
+    for (int i=0;i<N;++i) {
+        scanf("%d %d %d", &Fi[i],&Ei[i],&Pi[i]);
+        res += Pi[i];
     }
-
-    int F; cin >> F;
-    vi fails(F);
-    for (int t = 0; t < F; t++) cin >> fails[t];
-    sort(ALL(fails));
-
-    if (F == 0) {cout << res << endl; return 0;}
-
-    // orz bruce
-    // omg bro i forgot bout bin search 😭
-    for (int b = 0; b < B; b++) {
-        if (distance(lower_bound(ALL(fails),fv[b]),upper_bound(ALL(fails),ev[b]))) res -= pv[b];
-    }
-
-    cout << res << endl;
-
-    // map<pi,int> batches;
-    // for (int b=0;b<B;b++) {
-    //     int f, e, p; cin >> f >> e >> p;
-    //     batches[{f,e}] = p;
-    //     res += p;
-    // }
-
-    // int F; cin >> F;
-    // vi fails(F);
-    // for (int t = 0; t < F; t++) cin >> fails[t];
-    // sort(ALL(fails));
-
-    // if (F == 0) FOUNDANS(res);
-
-    // int i = 0;
-    // for (pair<pi,int> batch: batches) {
-    //     int l = batch.first.first, r = batch.first.second, p = batch.second;
-
-    //     bool failed = false;
-    //     for (; i < F; i++) {
-    //         if (fails[i] >= l && fails[i] <= r) {
-    //             if (!failed) {failed = true;}
-    //         }
-    //         else if (fails[i] > r) break;
-    //     }
-    //     if (failed) res -= p;
-    // }
-
     
+    int F; scanf("%d",&F);
+    if (F == 0) { printf("%d\n",res); return 0; }
+
+    vi fail(F);
+    for (int i=0;i<F;++i)
+        scanf("%d",&fail[i]);
+    sort(ALL(fail));
+
+    for (int i=0;i<N;++i) {
+        if (upper_bound(ALL(fail),Ei[i]) - lower_bound(ALL(fail),Fi[i]))
+            res -= Pi[i];
+    }
+
+    printf("%d\n",res);
 
     return 0;
 }
