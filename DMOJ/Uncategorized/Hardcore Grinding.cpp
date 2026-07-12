63a482f8089658bb63e7109d19d5e20f4a740ecd
dave <d4nplus6@gmail.com>
2026-07-12 12:19:57 -0400

    Optimized C++ sol for problem "Hardcore Grinding"

--- DMOJ/Uncategorized/Hardcore Grinding.cpp
+++ DMOJ/Uncategorized/Hardcore Grinding.cpp
@@ -7,34 +7,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef tuple<int ,int, int> ti;
-typedef vector<pi> vii;
-typedef vector<ti> vti;
-#define ALL(x) x.begin(), x.end()
+constexpr int MM = 1e7+1;
 
+int T[MM];
 int main() {
     int N; scanf("%d",&N);
-    vii T(N);
-    for (int i=0;i<N;++i)
-        scanf("%d %d", &T[i].first, &T[i].second);
-    sort(ALL(T));
-
-    priority_queue<pi, vii, greater<>> q;
-    q.push({T[0].second,0});
-    for (int i=1;i<N;++i) {
-        auto [f,m] = q.top();
-        if (T[i].first >= q.top().first) {
-            f = T[i].second;
-            q.pop();
-            q.push({f,m});
-        } else {
-            q.push({T[i].second,q.size()+1});
-        }
+    int latest = 0;
+    for (int s,f; N--;) {
+        scanf("%d %d",&s,&f);
+        ++T[s], --T[f];
+        latest = max(f, latest);
     }
 
-    printf("%d\n",(int)q.size());
+    int res = 0;
+    for (int p=0,i=1;i<latest;++i) {
+        p += T[i];
+        res = max(p,res);
+    }
+
+    printf("%d\n",res);
 
     return 0;
 }
