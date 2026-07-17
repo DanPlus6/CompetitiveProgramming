e9e61d26464c18335fc5af7904ae7a8a244f5bd0
dave <d4nplus6@gmail.com>
2026-07-17 18:56:27 -0400

    Fixed & optimized C++ sol for COCI '20 Contest 3 Problem 1

--- DMOJ/COCI/2020/Contest 3/COCI '20 Contest 3 #1 Knjige.cpp
+++ DMOJ/COCI/2020/Contest 3/COCI '20 Contest 3 #1 Knjige.cpp
@@ -14,43 +14,30 @@ typedef vector<int> vi;
 typedef vector<string> vs;
 #define pb push_back
 
-// UZMI for **take**
-// STAVI for **put**
-// L for **left**
-// D for **right**
-// format: take/put hand shelf
-
 int main() {
     int N; scanf("%d",&N);
-    if (N==1) return 0;
+    if (N==1) { printf("0\n"); return 0; }
 
-    stack<int> left; vi right;
-    for (int b,i=0;i<N;++i) {
-        scanf("%d",&b);
-        left.push(b);
-    }
-
-    vs res;
-    right.pb(left.top()); left.pop();
+    vi right(1); vs res;
+    scanf("%d",&right[0]);
     res.pb("UZMI D L\n"); res.pb("STAVI D D\n");
-    for (int nb; !left.empty();) {
-        nb = left.top(); left.pop();
+    for (int b, _=1;_<N;++_) {
+        scanf("%d",&b);
         res.pb("UZMI D L\n");
-        if (right.empty() || nb >= right.back()) {
-            right.pb(nb);
+        if (right.empty() || b >= right.back()) {
+            right.pb(b);
             res.pb("STAVI D D\n");
         } else {
             int i = right.size()-1;
-            for (;i >= 0 && nb < right[i];--i) {
+            for (;i >= 0 && b < right[i];--i) {
                 res.pb("UZMI L D\n");
                 res.pb("STAVI L L\n");
             }
             
             res.pb("STAVI D D\n");
-            i = right.size()-i-1;
-            right.insert(right.begin()+i+1,nb);
+            right.insert(right.begin()+i+1,b);
 
-            for (;i--;) {
+            for (i = right.size()-i-2;i--;) {
                 res.pb("UZMI D L\n");
                 res.pb("STAVI D D\n");
             }
