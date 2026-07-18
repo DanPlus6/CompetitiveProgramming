a29796680ca09c5e888c484a09f986e8dc1a324c
dave <d4nplus6@gmail.com>
2026-07-18 12:40:33 -0400

    Added C++ sol for Leetcode Biweekly Contest 187 Problem 1

---
+++ Leetcode/Contests/Biweekly Contest 187/Q1.py
@@ -0,0 +1,17 @@
+# Author: DanPlus6 // David Fu
+# Problem Name: Q1. Rearrange String to Avoid Character Pair
+# Problem Code: rearrange-string-to-avoid-character-pair
+# Problem URL:  https://leetcode.com/problems/rearrange-string-to-avoid-character-pair/description
+
+from collections import Counter
+class Solution:
+    def rearrangeString(self, s: str, x: str, y: str) -> str:
+        s = Counter(s)
+        res = ""
+        for _ in range(s[y]): res += y
+        for _ in range(s[x]): res += x
+        for k,v in s.items():
+            if k != x and k != y:
+                for _ in range(v): res += k
+
+        return res
