de3d6f4831abaf94906ee653e7c77d719b874e0d
dave <d4nplus6@gmail.com>
2026-07-19 09:16:23 -0400

    Added PY3 sol for Leetcode 3950 (from Biweekly contest 184)

---
+++ Leetcode/Contests/Biweekly Contest 184/Q1.py
@@ -0,0 +1,14 @@
+# Author: DanPlus6 // David Fu
+# Problem Name: Q1. Exactly One Consecutive Set Bits Pair
+# Problem Code: exactly-one-consecutive-set-bits-pair
+# Problem URL:  https://leetcode.com/problems/exactly-one-consecutive-set-bits-pair/description
+
+class Solution:
+    def consecutiveSetBits(self, n: int) -> bool:
+        if n <= 0: return False
+
+        pairs = n & (n >> 1)
+
+        return pairs > 0 and (pairs & (pairs - 1)) == 0
+        
+
