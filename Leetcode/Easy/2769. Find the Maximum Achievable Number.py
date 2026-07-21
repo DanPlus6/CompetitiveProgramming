6d21cfed08eb6fc97b55812549121a03c1cf6eae
dave <d4nplus6@gmail.com>
2026-07-21 08:39:24 -0400

    Added PY3 sol for Leetcode 2769

---
+++ Leetcode/Easy/2769. Find the Maximum Achievable Number.py
@@ -0,0 +1,7 @@
+# Author: DanPlus6 // David Fu
+# Problem Name: 2769. Find the Maximum Achievable Number
+# Problem Code: find-the-maximum-achievable-number
+# Problem URL:  https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int: return num+(t*2)
+        
