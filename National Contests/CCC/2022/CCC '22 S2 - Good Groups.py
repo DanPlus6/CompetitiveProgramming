65343cc9aa33adf54a068a5672b7306efb4a05f0
dave <d4nplus6@gmail.com>
2026-07-25 10:32:49 -0400

    Removed old code/comments in CCC '22 S2 PY3 sol

--- National Contests/CCC/2022/CCC '22 S2 - Good Groups.py
+++ National Contests/CCC/2022/CCC '22 S2 - Good Groups.py
@@ -1,5 +1,4 @@
 # CCC '22 S2 - Good Groups
-# efficient O(n) solution | Orz Bruce
 from sys import stdin
 X = int(stdin.readline())
 SAME = [list(stdin.readline().split()) for i in range(X)]
@@ -23,31 +22,3 @@ for rule in DIF:
 print(res)
 
 
-
-# Naieve solution
-"""
-X = int(stdin.readline())
-same = [list(stdin.readline().split()) for i in range(X)]
-Y = int(stdin.readline())
-dif = [list(stdin.readline().split()) for i in range(Y)]
-G = int(stdin.readline())
-PLC = [list(stdin.readline().split()) for i in range(G)]
-
-res = 0              
-for group in PLC:
-    rmS = []
-    for smR in same:
-        if (smR[0] in group and smR[1] not in group) or (smR[1] in group and smR[0] not in group):
-            res += 1
-            rmS.append(smR)
-    rmD = []
-    for difR in dif:
-        if difR[0] in group and difR[1] in group:
-            res += 1
-            rmD.append(difR)
-
-    for i in rmS: same.remove(i)
-    for i in rmD: dif.remove(i)
-
-print(res)
-"""
