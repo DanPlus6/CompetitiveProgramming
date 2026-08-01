8525bd0b44aedb12efa5f7f4fd909f16792532b9
David <d4nplus6@gmail.com>
2026-08-01 15:03:03 -0400

    Removed excessive coments in VMSS Pre-Windsor P7 sol

--- DMOJ/VMSS/Pre-Windsor/VMSS Pre-Windsor P7 - Going Back to the Definitions.cpp
+++ DMOJ/VMSS/Pre-Windsor/VMSS Pre-Windsor P7 - Going Back to the Definitions.cpp
@@ -10,18 +10,6 @@ int main() {
     vector<string> A(N);
     for (int i = 0; i < N; i++) {cin >> A[i];}
     sort(A.begin(),A.end(),[](string A, string B){
-        // 😭😭
-        // int n = A.size(); int m = B.size();
-        // for (int i = 0; i <min(n,m); i++) {
-        //     if (A[i] != B[i]) {return A[i] > B[i];}
-        // }
-        // return n < m;
-
-        // WAIT WHAT
-        // WHAT THE DUCK YOU CAN DO THIS
-        // WHY DO THEY LET YOU COMPARE STRINGS IN C++
-        // MY ENTIRE LIFE HAS BEEN A LIE
-        // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
         return A + B > B + A;
     });
 
