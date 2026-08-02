be59b5186f0ce876c2c9e25cea502631c3da2a32
David <d4nplus6@gmail.com>
2026-08-02 11:33:36 -0400

    Added C++ sol for Leetcode 877

---
+++ Leetcode/Medium/877. Stone Game.cpp
@@ -0,0 +1,16 @@
+/**
+ * Author: DanPlus6 // David Fu
+ * Problem Name: 877. Stone Game
+ * Problem URL:  https://leetcode.com/problems/stone-game/
+ * Problem Code: https://leetcode.com/problems/stone-game/
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGame(vector<int> &piles) {
+        return true;
+    }
+};
+
